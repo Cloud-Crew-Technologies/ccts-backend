2a898fbc99a3943504745e6ecee0e0e8bbb99050
tarakesh07 <tarakeshman@gmail.com>
2025-03-29 12:23:45 +0530

    small changes

--- Routes/profileroutes.js
+++ Routes/profileroutes.js
@@ -12,11 +12,11 @@ const router = express.Router();
 router.route("/login").post(loginprofile);
 router.route("/getallprofile").get(getAll);
 
-router.route("/:email").get(getUserbyID);
+router.route("/:name").get(getUserbyID);
 
 router.route("/create").post(newProfile);
 
-router.route("/:email").put(ProfileupdateById);
+router.route("/:name").put(ProfileupdateById);
 
 
 export default router;
